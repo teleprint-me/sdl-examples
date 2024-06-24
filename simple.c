1b99f476a72b18ba59b1603906a1cadd4d899153
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-24 13:54:45 -0400

    chore: Apply formatting and fix include directive
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- simple.c
+++ simple.c
@@ -1,34 +1,34 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Window *window =
-      SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-
-  if (!window) {
-    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Event e;
-  int quit = 0;
-
-  while (!quit) {
-    while (SDL_PollEvent(&e) != 0) {
-      if (e.type == SDL_QUIT) {
-        quit = 1;
-      }
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
     }
-  }
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN
+    );
 
-  return 0;
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Event e;
+    int       quit = 0;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
