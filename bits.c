596cb601af3fa3902e54c3c389fff12c1e4afc02
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-24 22:01:41 -0400

    refactor: Clean up precision typing implementation

--- bits.c
+++ bits.c
@@ -1,28 +1,38 @@
 #include <stdint.h>
 
+typedef union Float32 {
+    uint32_t as_bits;
+    float    as_value;
+} float32_t;
+
+// google brain half-precision bfloat16
+typedef struct BFloat16 {
+    uint16_t bits;
+} bfloat16_t;
+
+typedef uint16_t float16_t;
+
+bfloat16_t float_to_bfloat16(float value);
+float      bfloat16_to_float(bfloat16_t bf16);
+
 /**
  * Converts a floating-point number in bit representation to its equivalent
  * as a float value.
  */
-float fp32_from_bits(uint32_t bits);
+float float_from_bits(uint32_t bits);
 
 /**
  * Converts a floating-point number from the float format to its bit
  * representation.
  */
-uint32_t fp32_to_bits(float value);
-
-union FP32 {
-    uint32_t as_bits;
-    float    as_value;
-};
+uint32_t float_to_bits(float value);
 
 /**
  * Converts a floating-point number in bit representation to its equivalent
  * as a float value.
  */
-float fp32_from_bits(uint32_t bits) {
-    union FP32 fp32 = {.as_bits = bits};
+float float_from_bits(uint32_t bits) {
+    union Float32 fp32 = {.as_bits = bits};
     return fp32.as_value;
 }
 
@@ -30,7 +40,7 @@ float fp32_from_bits(uint32_t bits) {
  * Converts a floating-point number from the float format to its bit
  * representation.
  */
-uint32_t fp32_to_bits(float value) {
-    union FP32 fp32 = {.as_value = value};
+uint32_t float_to_bits(float value) {
+    union Float32 fp32 = {.as_value = value};
     return fp32.as_bits;
 }
