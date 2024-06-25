a418e3845a95fc47736435830ca680ed6cc7ef84
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-25 03:44:32 -0400

    chore: Remove structs and prototypes from implementation

--- precision.c
+++ precision.c
@@ -8,41 +8,6 @@
 #include "precision.h"
 #include <stdint.h>
 
-typedef enum DataType {
-    TYPE_FLOAT_F32,
-    TYPE_FLOAT_F16,
-    TYPE_FLOAT_BF16,
-    TYPE_QUANT_K8,
-    TYPE_QUANT_K4,
-    TYPE_MAX_COUNT, // number of data types
-} data_t;
-
-// 32-bit floating point (standard float)
-typedef union Float32 {
-    uint32_t as_bits;
-    float    as_value;
-} float32_t;
-
-// Google Brain half-precision bfloat16
-typedef uint16_t bfloat16_t;
-
-// Standard half-precision (IEEE 754)
-typedef uint16_t float16_t;
-
-// 8-bit quarter-precision (placeholder)
-typedef uint8_t quant8_t;
-
-// 4-bit eighth-precision (placeholder)
-// Here we might need to think differently as there's no native 4-bit type
-// and we might want to store two 4-bit values in one 8-bit variable.
-typedef uint8_t quant4_t;
-
-bfloat16_t float_to_bfloat16(float value);
-float      bfloat16_to_float(bfloat16_t bf16);
-
-float16_t float_to_float16(float value);
-float     float16_to_float(float16_t value);
-
 /**
  * Converts float32 to bfloat16.
  */
