873614ef559515764bdd09567c028d127b43c3e3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-06 12:49:32 -0400

    refactor: Add initial draft for dda implementation

--- dda.c
+++ dda.c
@@ -24,6 +24,16 @@ typedef struct IntegerPoint {
     int y;
 } int_point_t;
 
+// tolerance should be a default named parameter
+// where tolerance defaults to an argument of 0.5f
+float float_round(float value, float tolerance) {
+    return (value > (floor(value) + tolerance)) ? ceil(value) : floor(value);
+}
+
+int int_max(int x, int y) {
+    return abs(x) > abs(y) ? abs(x) : abs(y);
+}
+
 // Don't use globals! Globals are bad!
 // Apply Separation of Concerns (SoC) instead.
 int put_pixel(SDL_Renderer* renderer, int_point_t point) {
@@ -37,10 +47,35 @@ int put_pixel(SDL_Renderer* renderer, int_point_t point) {
     return SDL_RenderDrawPoint(renderer, point.x, point.y);
 }
 
+void dda_line(SDL_Renderer* renderer, float_point_t p_start, float_point_t p_end) {
+    int_point_t delta = {
+        .y = p_end.y - p_start.y,
+        .x = p_end.x - p_start.x,
+    };
+
+    int steps = int_max(delta.x, delta.y);
+
+    float_point_t increment = {
+        .y = delta.y / (float) steps,
+        .x = delta.x / (float) steps,
+    };
+
+    int_point_t current = {
+        .y = p_start.y,
+        .x = p_start.x,
+    };
+
+    for (int i = 0; i <= steps; i++) {
+        put_pixel(renderer, current);
+        current.y += float_round(increment.y, 0.5f);
+        current.x += float_round(increment.x, 0.5f);
+    }
+}
+
 int main(int argc, char* argv[]) {
-    // Points (2, 3) and (10, 8) are arbitrary and mostly for illustrative purposes
-    float_point_t p_start = {2, 3};  // x_1, y_1
-    float_point_t p_end   = {10, 8}; // x_2, y_2
+    // Points (0, 0) and (4, 5) are arbitrary and mostly for illustrative purposes
+    float_point_t p_start = {0.0f, 0.0f}; // x_1, y_1
+    float_point_t p_end   = {4.0f, 5.0f}; // x_2, y_2
 
     return 0;
 }
