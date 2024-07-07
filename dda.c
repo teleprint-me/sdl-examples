28196290ef42819400448c42e28682184283196c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-06 21:44:43 -0400

    feat: Add compatible sdl implementation for dda algorithm

--- dda.c
+++ dda.c
@@ -8,78 +8,111 @@
 
 #include <SDL2/SDL.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 
-// NOTE: SDL has defined this as struct SDL_FPoint
-// We use a custom point for illustrative purposes.
-// A structure representing a floating point
-typedef struct FloatingPoint {
+typedef struct {
     float x;
     float y;
 } float_point_t;
 
-// NOTE: SDL has defined this as struct SDL_Point
-// We use a custom point for illustrative purposes.
-// A structure representing an integer point
-typedef struct IntegerPoint {
-    int x;
-    int y;
-} int_point_t;
-
-// Function to round float values based on a given tolerance
-float float_round(float value, float tolerance) {
-    return (value > (floor(value) + tolerance)) ? ceil(value) : floor(value);
-}
-
 // Function to calculate the maximum of two integer values
-int int_max(int x, int y) {
+int get_steps(float_point_t delta) {
+    // Explicitly type cast to an integer so we can set the limit
+    int x = (int) delta.x;
+    int y = (int) delta.y;
+    // Return the limit based on the point's axis with the greater absolute value
     return abs(x) > abs(y) ? abs(x) : abs(y);
 }
 
-// Function to draw a pixel at a given point using SDL (integer coordinates)
-int put_pixel(SDL_Renderer* renderer, float_point_t point) {
-    // Cast the pixel coordinates from float to int
-    int_point_t pixel = {
-        .y = float_round(point.y, 0.5f),
-        .x = float_round(point.x, 0.5f),
-    };
-    // The SDL_RenderDrawPoint only accepts integers
-    // Otherwise we must use the SDL_RenderDrawPointF function instead
-    // 0 on success or a negative error code on failure
-    // call SDL_GetError() for more information
-    return SDL_RenderDrawPoint(renderer, pixel.x, pixel.y);
-}
-
-// Function to draw a line using the DDA algorithm
-void dda_line(SDL_Renderer* renderer, float_point_t p_start, float_point_t p_end) {
-    int_point_t delta = {
-        .y = p_end.y - p_start.y,
-        .x = p_end.x - p_start.x,
+void draw_line(SDL_Renderer* renderer, float_point_t start, float_point_t stop) {
+    // Calculate delta values
+    float_point_t delta = {
+        .y = stop.y - start.y,
+        .x = stop.x - start.x,
     };
 
-    int steps = int_max(delta.x, delta.y);
+    // Calculate the number of steps required
+    int steps = get_steps(delta);
 
+    // Calculate increment values
     float_point_t increment = {
         .y = delta.y / (float) steps,
         .x = delta.x / (float) steps,
     };
 
-    float_point_t current = {
-        .y = p_start.y,
-        .x = p_start.x,
-    };
+    // Initialize current point to start point
+    float_point_t current = start;
 
+    // Loop through and plot each point
     for (int i = 0; i <= steps; i++) {
-        put_pixel(renderer, current);
+        SDL_RenderDrawPointF(renderer, current.x, current.y);
         current.y += increment.y;
         current.x += increment.x;
     }
 }
 
 int main(int argc, char* argv[]) {
+    // init sdl
+    if (SDL_Init(SDL_INIT_VIDEO)) { // errors on truthy values
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // create sdl window
+    SDL_Window* window = SDL_CreateWindow(
+        "DDA Line Drawing",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+    if (NULL == window) {
+        fprintf(stderr, "SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // create sdl renderer
+    SDL_Renderer* renderer
+        = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (NULL == renderer) {
+        SDL_DestroyWindow(window);
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // set the background to black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    // clear the window
+    SDL_RenderClear(renderer);
+
+    // define start and end points here
     // Points (0, 0) and (4, 5) are arbitrary and mostly for illustrative purposes
-    float_point_t p_start = {0.0f, 0.0f}; // x_1, y_1
-    float_point_t p_end   = {4.0f, 5.0f}; // x_2, y_2
+    // we draw a line from the origin to the center of the window
+    // origin = start = (0, 0)
+    // center = stop  = (640 / 2, 480 / 2) = (320, 240)
+    float_point_t start = {0.0f, 0.0f};     // x_1, y_1
+    float_point_t stop  = {320.0f, 240.0f}; // x_2, y_2
+
+    // set the line color to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+    // draw the line using dda algo
+    draw_line(renderer, start, stop);
+
+    // present the renderer
+    SDL_RenderPresent(renderer);
+
+    // wait for a few seconds before quitting
+    SDL_Delay(5000);
+
+    // cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
