2a8659b2ced8833464cfa8e8f794974cfa354aeb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-15 01:16:13 -0400

    primitives(feat): Add initial tensor implementation

--- primitives.c
+++ primitives.c
@@ -28,14 +28,14 @@ vector_t* create_vector(size_t size) {
 
 void free_vector(vector_t* vector) {
     if (NULL == vector) {
-        fprintf(stderr, "Cannot free a NULL struct Vector.\n");
+        fprintf(stderr, "Cannot free a NULL vector.\n");
+        return;
     }
 
-    if (NULL == vector->elements) {
-        fprintf(stderr, "Cannot free a NULL vector->elements member.\n");
+    if (vector->elements) {
+        free(vector->elements);
     }
 
-    free(vector->elements);
     free(vector);
 }
 
@@ -81,9 +81,9 @@ void free_matrix(matrix_t* matrix) {
         return;
     }
 
-    if (NULL != matrix->elements) {
+    if (matrix->elements) {
         for (size_t i = 0; i < matrix->n_rows; ++i) {
-            if (NULL != matrix->elements[i]) {
+            if (matrix->elements[i]) {
                 free(matrix->elements[i]);
             }
         }
@@ -94,8 +94,61 @@ void free_matrix(matrix_t* matrix) {
 }
 
 // Tensor operations
-tensor_t create_tensor(size_t* dimensions, rank_t rank);
-void     free_tensor(tensor_t* tensor);
+tensor_t* create_tensor(size_t* dimensions, rank_t rank) {
+    if (rank == SCALAR || rank >= MAX_RANK) {
+        fprintf(stderr, "Invalid rank for tensor. Must be between 1 and %d.\n", MAX_RANK - 1);
+        return NULL;
+    }
+
+    tensor_t* tensor = (tensor_t*) malloc(sizeof(tensor_t));
+    if (NULL == tensor) {
+        fprintf(stderr, "Failed to allocate memory for tensor_t.\n");
+        return NULL;
+    }
+
+    tensor->rank       = rank;
+    tensor->dimensions = (size_t*) malloc(rank * sizeof(size_t));
+    if (NULL == tensor->dimensions) {
+        fprintf(stderr, "Failed to allocate memory for tensor dimensions.\n");
+        free(tensor);
+        return NULL;
+    }
+
+    size_t total_elements = 1;
+    for (size_t i = 0; i < rank; ++i) {
+        tensor->dimensions[i]  = dimensions[i];
+        total_elements        *= dimensions[i];
+    }
+
+    tensor->elements = (float*) malloc(total_elements * sizeof(float));
+    if (NULL == tensor->elements) {
+        fprintf(stderr, "Failed to allocate memory for tensor elements.\n");
+        free(tensor->dimensions);
+        free(tensor);
+        return NULL;
+    }
+
+    memset(tensor->elements, 0, total_elements * sizeof(float));
+
+    return tensor;
+}
+
+void free_tensor(tensor_t* tensor) {
+    if (NULL == tensor) {
+        fprintf(stderr, "Cannot free a NULL tensor.\n");
+        return;
+    }
+
+    if (tensor->dimensions) {
+        free(tensor->dimensions);
+    }
+
+    if (tensor->elements) {
+        free(tensor->elements);
+    }
+
+    free(tensor);
+}
 
 // Line segment operations
 line_t create_line(size_t size) {
