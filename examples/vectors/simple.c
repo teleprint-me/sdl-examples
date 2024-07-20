2ad0a4e2c7e86df2a450cd6ae6ea536543e63a53
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-20 02:26:53 -0400

    vector(simple): Fix simple vector example

--- examples/vectors/simple.c
+++ examples/vectors/simple.c
@@ -20,20 +20,20 @@ size_t calculate_vector_magnitude(SDL_FPoint* point) {
 int main(int argc, char* argv[]) {
     size_t dimensions = 2; // 2-dimensional vector
 
-    vector_t* a        = vector_create(dimensions);
-    a->displacement[X] = 1.5f;
-    a->displacement[Y] = 2.75f;
+    vector_t* a    = vector_create(dimensions);
+    a->elements[X] = 1.5f;
+    a->elements[Y] = 2.75f;
 
-    vector_t* b        = vector_create(dimensions);
-    b->displacement[X] = 3.25f;
-    b->displacement[Y] = 1.25f;
+    vector_t* b    = vector_create(dimensions);
+    b->elements[X] = 3.25f;
+    b->elements[Y] = 1.25f;
 
     vector_t* c = vector_vector_add(a, b);
 
     for (size_t i = 0; i < dimensions; i++) {
-        fprintf(stderr, "a->elements[%zd] = %.2f\n", i, a->displacement[i]);
-        fprintf(stderr, "b->elements[%zd] = %.2f\n", i, b->displacement[i]);
-        fprintf(stderr, "c->elements[%zd] = %.2f\n", i, c->displacement[i]);
+        fprintf(stderr, "a->elements[%zd] = %.2f\n", i, a->elements[i]);
+        fprintf(stderr, "b->elements[%zd] = %.2f\n", i, b->elements[i]);
+        fprintf(stderr, "c->elements[%zd] = %.2f\n", i, c->elements[i]);
     }
 
     vector_free(a);
